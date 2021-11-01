a50b1135d298a6219d54708089acaadbde6b19f0
Mohamed Aissa Benfodda <rygaa@protonmail.com>
2021-10-31 23:40:10 -0500

    Changing port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3006
 
 app.use(express.static('build'));
 
