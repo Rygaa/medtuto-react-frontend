4a971fd58051c0c7e0edbb8df998eafaa07f3a31
Mohamed Aissa Benfodda <rygaa@protonmail.com>
2022-01-02 14:54:42 -0500

    re-add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+const PORT = process.env.PORT || 3006
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log('Server listening to', PORT);
+})
+
+
